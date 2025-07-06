9b9a253d97ca6d574ec04173cef217c4207dc57f
Samyla Nascimento Silva <112506333+Samylanascimento@users.noreply.github.com>
2025-07-06 20:12:40 -0300

    idade-dias Q2.py

--- Q2.py
+++ Q2.py
@@ -1,12 +1,28 @@
+from datetime import date
 
+def calcular_idade_em_dias(ano_nascimento):
 
-def soma_numeros():
-  numero1 = 12
-  numero2 = 14
-  soma = numero1 + numero2
+    ano_atual = date.today().year
+    
+    if ano_nascimento > ano_atual or ano_nascimento <= 0:
+        return None 
 
-  print(f"Número 1: {numero1}")
-  print(f"Número 2: {numero2}")
-  print(f"Soma: {soma}")
+    idade_em_anos = ano_atual - ano_nascimento
+    idade_em_dias = idade_em_anos * 365
+    
+    return idade_em_dias
 
-soma_numeros()
+try:
+    nascimento = int(input("Digite o seu ano de nascimento (com 4 dígitos): "))
+    
+    idade_dias = calcular_idade_em_dias(nascimento)
+
+    if idade_dias is not None:
+      
+        print(f"Você tem aproximadamente {idade_dias:,d} dias de vida.".replace(',', '.'))
+        print("\nNota: Este é um valor aproximado que não considera os dias extras de anos bissextos.")
+    else:
+        print("Ano de nascimento inválido.")
+
+except ValueError:
+    print("Erro: Por favor, digite um ano válido (somente números).")
